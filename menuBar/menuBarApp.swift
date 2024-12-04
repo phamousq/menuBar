e2f04e51c15efb290c7928252c6887fa4aa5f7cc
q <phamousq@gmail.com>
2024-12-04 16:28:06 -0600

    Initial Commit

---
+++ menuBar/menuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuBarApp.swift
+//  menuBar
+//
+//  Created by Quinton Pham on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct menuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
